8204ca86f6173cdc5a1db266c5a8087d0d2773ad
Константин Лещук <mydocuments@MacBook-Air-Konstantin.local>
2025-04-03 00:27:02 +0400

    chore: configure build system (webpack, eslint, bootstrap, makefile, vercel)

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+
+export default {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'main.js',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  devServer: {
+    static: './dist',
+    open: true,
+    port: 8080,
+  },
+  module: {
+    rules: [],
+  },
+  mode: 'development',
+};
